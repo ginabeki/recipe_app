aa81bba09d87e37bec81a4860273a7ca0dea4587
ginabeki <ginabeki3@gmail.com>
2023-03-15 12:29:12 +0300

    Change route for foods to index

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "users#index"
+  root "foods#index"
   
-  resources :users, only: [:index, :show] do
-    resources :foods, only: [:index, :new, :create, :destroy]
+  resources :foods, only: [:index, :new, :create, :destroy]
+  resources :users, only: [:index]
     resources :recipes, only: [:index, :new, :create, :show, :destroy]
-  end
+  # end
   
 end

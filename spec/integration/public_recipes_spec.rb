df149d63e62b9170c47ac905b33014c0a4f40be3
ginabeki <ginabeki3@gmail.com>
2023-03-18 15:31:23 +0300

    Add test suite for public recipes

--- spec/integration/public_recipes_spec.rb
+++ spec/integration/public_recipes_spec.rb
@@ -1,6 +1,15 @@
 require 'rails_helper'
 describe '/public_recipes', type: :feature do
-  scenario 'guest can see but not view a page' do
+  let(:user) { FactoryBot.create(:user) }
+  scenario 'user can see and view a page' do
+    user.confirm # Confirm the user's email address
+    visit new_user_session_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    click_button 'Log in'
+    expect(page).to have_text('Signed in successfully.')
+    recipe = FactoryBot.create(:recipe, public: true)
+    recipe2 = FactoryBot.create(:recipe, public: false)
     visit '/public_recipes'
     expect(page).to have_text('Public Recipes')
   end
